a28a0f3eaf47ff32684f2fad97456a97dfb7dc0f
Onurhan <onurhanbucak@gmail.com>
2026-05-14 21:00:19 +0300

    Initial Commit

---
+++ Kuyumcu/KuyumcuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuyumcuApp.swift
+//  Kuyumcu
+//
+//  Created by bucak architects on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct KuyumcuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
